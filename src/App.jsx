73c3749628df3c89836f85153b21c90ee48ad2c5
Soumya-0x000 <soumyadas429@gmail.com>
2023-12-11 11:25:23 +0530

    Few changes are made

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import { getApiConfiguration, getGenres } from './store/homeSlice'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
-import Details from './pages/details/details'
-import SearchResult from './pages/searchResult/searchResult'
-import Explore from './pages/explore/explore'
+import Details from './pages/details/Details'
+import SearchResult from './pages/searchResult/SearchResult'
+import Explore from './pages/explore/Explore'
 import PageNotFound from './pages/404/PageNotFound'
 
 
