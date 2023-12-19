fb0d95f136b9f0fe07ef7b4d1372ea23117d8e31
Soumya-0x000 <soumyadas429@gmail.com>
2023-12-20 00:18:01 +0530

    person field is under construction

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import HeroBanner from './heroBanner/HeroBanner'
 import Trending from './trending/Trending'
 import Popular from './popular/Popular'
 import TopRated from './topRated/TopRated'
+import Person from './person/Person'
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Trending/>
             <Popular/>
             <TopRated/>
+            <Person/>
         </div>
     )
 }
--- src/pages/home/person/Person.jsx
+++ src/pages/home/person/Person.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+import useFetch from '../../../hooks/useFetch'
+import { useSelector } from 'react-redux'
+import noImg from '../../../assets/no-poster.png'
+import Img from '../../../components/lazyLoadImage/Img'
 
 const Person = () => {
+    const { data, loading } = useFetch(`/person/changes`)
+    const { data: personData, loading: isPersonLoading } = useFetch(`/person/popular`)
+    const { url } = useSelector((state) => state.home)
+    // console.log(data);
+    console.log(personData);
+    console.log(url?.poster);
+
     return (
-        <div>Person</div>
+        <div>
+            <ContentWrapper>
+                <div className='flex items-center justify-between mb-[20px] '>
+                    <span className='text-[24px] text-white font-medium'>
+                        Popular Person
+                    </span>
+                </div>
+
+                {!isPersonLoading ? (
+                    <div className='flex gap-[10px] md:gap-5 overflow-x-auto mb-8'>
+                        {personData?.results?.map((item, index) => {
+                            console.log(item);
+                            const posterUrl = item?.profile_path
+                                ? url.poster + item?.profile_path 
+                                : noImg;
+                            return(
+                                <div key={index} className='text-white'>
+                                    <div className='w-[125px] md:w-[175px] h-[125px] md:h-[175px] object-cover object-center rounded-full overflow-hidden '>
+                                        <Img src={posterUrl} className={`w-full h-full `}/>
+                                    </div>
+                                    <div className="name mt-4 text-[14px] md:text-[18px] leading-5 md:left-6 font-bold ">
+                                        {item.name}
+                                    </div>
+                                    <div className="character text-[14px] md:text-[16px] leading-5 mt-2 md:left-6 opacity-50">
+                                        {item.known_for_department}
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </ContentWrapper>
+        </div>
     )
 }
 
